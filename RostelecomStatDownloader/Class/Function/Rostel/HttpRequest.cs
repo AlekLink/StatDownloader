5baf3aaea1b1d91d1f03e39c247c32b90db45346
AlekLink <a.shepelkin@gmail.com>
2014-09-08 12:40:15 +0400

    Start work with download stat

---
+++ RostelecomStatDownloader/Class/Function/Rostel/HttpRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+
+namespace StatDownloader.Class.Function.Rostel
+{
+    public class HttpRequest
+    {
+        
+
+        public HttpRequest()
+        {
+
+        }
+
+        public void getData()
+        {
+            
+        }
+
+    }
+}
